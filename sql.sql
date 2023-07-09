b41fb2af884c20ac492a49a6cefcde36713c9ee2
GabrielHR0sa <rosahenriquegabriel@gmail.com>
2023-07-09 13:27:03 -0300

    add the sql file

---
+++ sql.sql
@@ -0,0 +1,20 @@
+INSERT INTO users(id, email, first_name, last_name, password)
+VALUES (1, 'operator@gmail.com', 'Gabriel', 'Rosa', '$2a$10$eUr7IPl2bWVh9eK1wq358ONgz5HrguxYf/XUQedZLDcMXW5e273cW');
+
+INSERT INTO users(id, email, first_name, last_name, password)
+VALUES (2, 'admin@gmail.com', 'admin', 'admin', '$2a$10$a0R.lrF//bJlt/b817j60OxlX8COwYw.Hs22LpKdUGD5wNJqN.VAi');
+
+INSERT INTO roles(id, name)
+VALUES (1, 'ROLE_OPERATOR');
+
+INSERT INTO roles(id, name)
+VALUES (2, 'ROLE_ADMIN');
+
+INSERT INTO user_role(user_id, role_id)
+VALUES (1, 1);
+
+INSERT INTO user_role(user_id, role_id)
+VALUES (2, 2);
+
+
+
